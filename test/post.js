d7f82ee384f5c2a19b2200045f18947df4477d27
Damian Suarez <rdsuarez@gmail.com>
2014-04-14 13:15:27 -0300

    test: update post tests

--- test/post.js
+++ test/post.js
@@ -61,7 +61,7 @@ describe('WPCOM#Site#Post', function(){
   describe('async', function(){
 
     describe('get', function(){
-      it('should get the recently added post', function(done){
+      it('should get added post (by id)', function(done){
         var site = util.private_site();
         var post = site.post(new_post.ID);
 
@@ -73,11 +73,11 @@ describe('WPCOM#Site#Post', function(){
         });
       });
 
-      it('should get by slug', function(done){
+      it('should get added post (by slug)', function(done){
         var site = util.private_site();
         var post = site.post({ slug: new_post.slug });
 
-        post.getbyslug(function(err, post){
+        post.get(function(err, post){
           if (err) throw err;
 
           post.should.be.eql(new_post);
@@ -90,19 +90,21 @@ describe('WPCOM#Site#Post', function(){
     describe('add', function(){
 
       it('should add a new post', function(done){
-        var wpcom = util.private_site();
-        wpcom.site.post.add(test.new_post_data, function(err, post){
+        var site = util.private_site();
+        var post = site.post();
+
+        post.add(test.new_post_data, function(err, data){
           if (err) throw err;
 
-          // checking some post date
-          post
+          // checking some data date
+          data
             .should.be.ok
             .and.be.an.instanceOf(Object);
 
-          post.ID
+          data.ID
             .should.be.an.instanceOf(Number);
 
-          post.site_ID
+          data.site_ID
             .should.be.an.instanceOf(Number)
             .and.be.eql(test.private_site_id);
 
@@ -115,17 +117,19 @@ describe('WPCOM#Site#Post', function(){
     describe('edit', function(){
 
       it('should edit the new added post', function(done){
-        var wpcom = util.private_site();
+        var site = util.private_site();
+        var post = site.post(new_post.ID);
+
         var edited_title = new_post.title + ' has been changed';
 
-        wpcom.site.post.edit(new_post.ID, { title: edited_title }, function(err, post){
+        post.edit({ title: edited_title }, function(err, data){
           if (err) throw err;
 
-          post
+          data
             .should.be.ok
             .and.be.an.instanceOf(Object);
 
-          post.title
+          data.title
             .should.be.eql(edited_title);
 
           done();
