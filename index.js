6b86f8436fb1234e206b5706c1f58af96e564b7a
Damian Suarez <rdsuarez@gmail.com>
2014-04-22 11:39:05 -0300

    core: default method `GET` instead of `get`

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ WPCOM.prototype.sendRequest = function (type, vars, params, fn){
   var end = ends(type);
 
   // request method
-  var method = (params.method || end.method || 'get').toLowerCase();
+  var method = (params.method || end.method || 'GET').toLowerCase();
   delete params.method;
   debug('method: `%s`', method);
 
