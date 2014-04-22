f5a056bb924c7db45718adf5dba5e3a60479acbf
Nathan Rajlich <nathan@tootallnate.net>
2014-04-21 18:52:44 -0700

    index: remove `req.js`, move send() fn to WPCOM class
    There is no need for the Req class…

--- index.js
+++ index.js
@@ -5,24 +5,19 @@
 
 var Me = require('./lib/me');
 var Sites = require('./lib/sites');
-
-var req = require('./lib/req');
+var ends = require('./lib/endpoint');
 var debug = require('debug')('wpcom');
 
 /**
- * WordPress REST-API class
+ * WordPress.com REST API class.
  *
- * @param {String} [token]
  * @api public
  */
 
-function WPCOM(token){
-  if (!(this instanceof WPCOM)) return new WPCOM(token);
-
-  this.tkn = token;
-
-  // request instance
-  this.req = new req(this);
+function WPCOM(request){
+  if (!(this instanceof WPCOM)) return new WPCOM(request);
+  if ('function' !== typeof request) throw new TypeError('a `request` WP.com function must be passed in');
+  this.request = request;
 }
 
 /**
@@ -32,7 +27,7 @@ function WPCOM(token){
  */
 
 WPCOM.prototype.me = function(){
-  return Me(this);
+  return new Me(this);
 };
 
 /**
@@ -43,7 +38,65 @@ WPCOM.prototype.me = function(){
  */
 
 WPCOM.prototype.sites = function(id){
-  return Sites(id, this);
+  return new Sites(id, this);
+};
+
+/**
+ * Request to WordPress REST API
+ *
+ * @param {String} type endpoint type
+ * @param {Object} vars to build endpoint
+ * @param {Object} params
+ * @param {Function} fn
+ * @api private
+ */
+
+WPCOM.prototype.sendRequest = function (type, vars, params, fn){
+  console.log(type, vars, params);
+  debug('type: `%s`', type);
+
+  // token
+  var token = params.token || this.token;
+  delete params.token;
+
+  // headers
+  var headers = {};
+  if (!token) {
+    debug('WARN: token is not defined');
+  } else {
+    headers.authorization = "Bearer " + token;
+  }
+
+  // options object || callback function
+  if ('function' == typeof params) {
+    fn = params;
+    params = {};
+  }
+
+  if (!params) params = {};
+  if (!fn) fn = function(err){ if (err) throw err; };
+
+  // endpoint config object
+  var end = ends(type);
+  console.log(end);
+
+  // request method
+  var method = (params.method || end.method || 'get').toLowerCase();
+  delete params.method;
+  debug('method: `%s`', method);
+
+  // build endpoint url
+  var endpoint = end.path;
+  if (vars) {
+    for (var k in vars) {
+      var rg = new RegExp("%" + k + "%");
+      endpoint = endpoint.replace(rg, vars[k]);
+    }
+  }
+  params.path = endpoint;
+  debug('endpoint: `%s`', endpoint);
+
+  this.request(params, fn);
 };
 
 /**
