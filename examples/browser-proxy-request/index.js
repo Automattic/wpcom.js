462cb6e28ab75d353966145644d6aee8c45b244d
Damian Suarez <rdsuarez@gmail.com>
2014-04-24 11:51:48 -0300

    example: clean app in browser-proxy example

--- examples/browser-proxy-request/index.js
+++ examples/browser-proxy-request/index.js
@@ -5,18 +5,10 @@
 
 var express = require('express');
 
-// setup middleware
-
 var app = express();
-var pub = __dirname + '/public';
-app.use(express.static(pub));
+app.use(express.static(__dirname + './../../dist'));
+app.use(express.static(__dirname));
 
-app.set('views', __dirname + '/');
-app.set('view engine', 'jade');
-
-app.get('/', function(req, res){
-  res.render('index');
-});
-
-app.listen(80);
-console.log('wpcom app started on port 3000');
+var port = 80;
+app.listen(port);
+console.log('wpcom app started on port %s', port);
