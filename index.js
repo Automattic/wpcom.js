b2176e387da67d3f13620fc9ec93f00509e2a465
Radford Smith <contact@radsmith.com>
2014-11-25 13:07:35 -0500

    Restore previously removed methods to keep backwards compatability

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-
 /**
  * Module dependencies.
  */
 
 var WPCOM = require('./wpcom');
 var request = require('wpcom-xhr-request');
+var inherits = require('inherits');
 
 /**
+ * WordPress.com REST API class.
+ *
  * XMLHttpRequest (and CORS) API access method.
  *
  * API authentication is done via an (optional) access `token`,
@@ -18,10 +20,36 @@ var request = require('wpcom-xhr-request');
  * @param {String} [token] - OAuth API access token
  * @public
  */
+function XhrWPCOM(token) {
+  if (!(this instanceof WPCOM)) return new WPCOM(token);
+  WPCOM.call(this, request);
+  this.token = token;
+}
 
-module.exports = function(token){
-  return new WPCOM(function(params, fn){
-    params.authToken = token;
-    return request(params, fn);
-  });
+inherits(XhrWPCOM, WPCOM);
+
+/**
+ * Set access token.
+ *
+ * @param {String} token - API token to use for requests
+ * @public
+ */
+XhrWPCOM.prototype.setToken = function(token) {
+  this.token = token;
 };
+
+/**
+ * Overwrite the parent `sendRequest()` function so that we can
+ * add the `authToken` to every API request if it's present.
+ *
+ * @api private
+ */
+XhrWPCOM.prototype.sendRequest = function (params, query, body, fn) {
+  if ('string' == typeof params) params = { path: params };
+  // token
+  var token = params.token || this.token;
+  if (token) params.authToken = token;
+  return WPCOM.prototype.sendRequest.call(this, params, query, body, fn);
+};
+
+module.exports = XhrWPCOM;
