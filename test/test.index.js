a90e845e173fca27c30e3ed5cfc1c2b3867949d1
Damian Suarez <rdsuarez@gmail.com>
2015-02-04 19:24:05 -0300

    test: improve core tests

--- test/test.index.js
+++ test/test.index.js
@@ -7,33 +7,23 @@ var WPCOM = require('../');
 var assert = require('assert');
 
 /**
- * Testing data
- */
-
-var test = require('./data');
-var util = require('./util');
-
-/**
  * WPCOM object
  */
 
-describe('WPCOM', function(){
+describe('wpcom', function(){
 
-  describe('async', function(){
+  describe('wpcom.freshlyPressed()', function(){
+    it('should require freshly pressed', function(done){
+      var wpcom = WPCOM();
 
-    describe('freshlyPressed()', function(){
-      it('should require freshly pressed', function(done){
-        var wpcom = util.wpcom();
+      wpcom.freshlyPressed(function(err, data){
+        if (err) throw err;
 
-        wpcom.freshlyPressed(function(err, data){
-          if (err) throw err;
-
-          // testing object
-          assert.ok(data);
-          assert.equal('number', typeof data.number);
-          assert.ok(data.posts instanceof Array);
-          done();
-        });
+        // testing object
+        assert.ok(data);
+        assert.equal('number', typeof data.number);
+        assert.ok(data.posts instanceof Array);
+        done();
       });
 
     });
