d50e5e3a486097b51991111fb2cf3b27fc8c2b58
Damian Suarez <rdsuarez@gmail.com>
2015-02-05 10:08:05 -0300

    test: rename, clean and tidy tag test

--- test/test.wpcom.site.tag.js
+++ test/test.wpcom.site.tag.js
@@ -18,22 +18,24 @@ describe('wpcom.site.tag', function(){
   var wpcom = WPCOM(fixture.site.token);
   var site = wpcom.site(fixture.site.url);
 
-  var testing_tag;
-
-  // Create a new_tag before to start tests
+  // global var to store new tag
   var new_tag;
+
+  // Create a testing_tag before to start tests
+  var testing_tag;
   before(function(done){
     site.tag()
     .add(fixture.tag, function(err, tag) {
       if (err) throw err;
 
-      new_tag = tag;
+      testing_tag = tag;
       done();
     });
   });
 
+  // Delete testing tag
   after(function(done){
-    site.tag(new_tag.slug)
+    site.tag(testing_tag.slug)
     .delete(function(err, tag) {
       if (err) throw err;
 
@@ -44,15 +46,15 @@ describe('wpcom.site.tag', function(){
   describe('wpcom.site.tag.get()', function(){
 
     it('should get added tag', function(done){
-      var cat = site.tag(new_tag.slug);
+      var cat = site.tag(testing_tag.slug);
 
       cat.get(function(err, data){
         if (err) throw err;
 
         assert.ok(data);
         assert.ok(data instanceof Object, 'data is not an object');
-        assert.equal(new_tag.slug, data.slug);
-        assert.equal(new_tag.name, data.name);
+        assert.equal(testing_tag.slug, data.slug);
+        assert.equal(testing_tag.name, data.name);
         done();
       });
     });
@@ -72,7 +74,7 @@ describe('wpcom.site.tag', function(){
         assert.ok(data instanceof Object, 'data is not an object');
 
         // store added catogory
-        testing_tag = data;
+        new_tag = data;
 
         done();
       });
@@ -82,7 +84,7 @@ describe('wpcom.site.tag', function(){
   describe('wpcom.site.tag.update()', function(){
 
     it('should edit the new added tag', function(done){
-      var tag = site.tag(testing_tag.slug);
+      var tag = site.tag(new_tag.slug);
       var edited_name = fixture.tag.name + '-edited';
 
       tag.update({ name: edited_name }, function(err, data){
@@ -92,7 +94,7 @@ describe('wpcom.site.tag', function(){
         assert.equal(edited_name, data.name);
 
         // update added tag
-        testing_tag = data;
+        new_tag = data;
 
         done();
       });
@@ -102,14 +104,14 @@ describe('wpcom.site.tag', function(){
   describe('wpcom.site.tag.delete()', function() {
 
     it('should delete the new added tag', function(done) {
-      var cat = site.tag(testing_tag.slug);
+      var cat = site.tag(new_tag.slug);
 
       cat.delete(function(err, data) {
         if (err) throw err;
 
         assert.ok(data);
         assert.equal("true", data.success);
-        assert.equal(testing_tag.slug, data.slug);
+        assert.equal(new_tag.slug, data.slug);
 
         done();
       });
