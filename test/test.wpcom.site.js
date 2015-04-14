bc0cb4184a717ec6fca73a0e69104483c6479275
Timmy Crawford <timmydcrawford@gmail.com>
2015-04-14 14:19:05 -0700

    Adding remaining stats list endpoints.

--- test/test.wpcom.site.js
+++ test/test.wpcom.site.js
@@ -226,6 +226,144 @@ describe('wpcom.site', function () {
       });
     });
 
+    describe('wpcom.site.statsComments', function () {
+      it('should request comments data', function (done) {
+        site.statsComments(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.ok(data.authors instanceof Array);
+          assert.ok(data.posts instanceof Array);
+          assert.equal('number', typeof data.monthly_comments);
+          assert.equal('number', typeof data.total_comments);
+          assert.equal('string', typeof data.most_active_day);
+          assert.equal('string', typeof data.most_active_time);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsCommentFollowers', function () {
+      it('should request comment follower data', function (done) {
+        site.statsCommentFollowers(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.ok(data.posts instanceof Array);
+          assert.equal('number', typeof data.page);
+          assert.equal('number', typeof data.pages);
+          assert.equal('number', typeof data.total);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsFollowers', function () {
+      it('should request follower data', function (done) {
+        site.statsFollowers(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.ok(data.subscribers instanceof Array);
+          assert.equal('number', typeof data.page);
+          assert.equal('number', typeof data.pages);
+          assert.equal('number', typeof data.total);
+          assert.equal('number', typeof data.total_email);
+          assert.equal('number', typeof data.total_wpcom);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsPublicize', function () {
+      it('should request publicize data', function (done) {
+        site.statsPublicize(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.ok(data.services instanceof Array);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsStreak', function () {
+      it('should request streak data', function (done) {
+        site.statsStreak(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.ok(data.streak instanceof Object);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsSummary', function () {
+      it('should request summary data', function (done) {
+        site.statsSummary(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.equal('string', typeof data.period);
+          assert.equal('number', typeof data.likes);
+          assert.equal('number', typeof data.views);
+          assert.equal('number', typeof data.visitors);
+          assert.equal('number', typeof data.comments);
+          assert.equal('number', typeof data.followers);
+          assert.equal('number', typeof data.reblogs);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsTags', function () {
+      it('should request tag data', function (done) {
+        site.statsTags(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.ok(data.tags instanceof Array);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsTopAuthors', function () {
+      it('should request author data', function (done) {
+        site.statsTopAuthors(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.equal('string', typeof data.period);
+          assert.ok(data.days instanceof Object);
+
+          done();
+        });
+      });
+    });
+
+    describe('wpcom.site.statsVideoPlays', function () {
+      it('should request video play data', function (done) {
+        site.statsVideoPlays(function (err, data) {
+          if (err) throw err;
+
+          assert.equal('string', typeof Date(data.day));
+          assert.equal('string', typeof data.period);
+          assert.ok(data.days instanceof Object);
+
+          done();
+        });
+      });
+    });
+
     describe('wpcom.site.statsVisits', function () {
       it('should request visits stats', function (done) {
         site.statsVisits(function (err, data) {
