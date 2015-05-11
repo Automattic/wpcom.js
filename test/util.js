f4c4a2d46c076ae98e77c29c3835ebd573d3825b
retrofox <rdsuarez@gmail.com>
2015-05-10 19:00:53 -0700

    test: add util

---
+++ test/util.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies
+ */
+
+var WPCOM = require('../');
+
+/**
+ * Detect client/server side
+ */
+
+var is_client_side = 'undefined' !== typeof window;
+
+/**
+ * Testing data
+ */
+
+var fixture = require('./fixture');
+
+module.exports = {
+  wpcom: wpcom
+};
+
+function wpcom() {
+  if (is_client_side) {
+    var proxy = require('../node_modules/wpcom-proxy-request');
+    var _wpcom = WPCOM(proxy);
+
+    _wpcom.request({
+      metaAPI: { accessAllUsersBlogs: true }
+    }, function(err) {
+      if (err) throw err;
+      console.log('proxy now running in "access all user\'s blogs" mode');
+    });
+
+    return _wpcom;
+  } else {
+    // get token from environment var
+    var token = process.env.TOKEN;
+    return WPCOM(token);
+  }
+}
