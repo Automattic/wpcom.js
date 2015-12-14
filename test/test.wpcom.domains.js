de3df32f097cd71e71ef69e8fc692aa284f37d0f
retrofox <rdsuarez@gmail.com>
2015-12-14 16:00:44 -0300

    test: add wpcom.domains tests

---
+++ test/test.wpcom.domains.js
@@ -0,0 +1,74 @@
+/**
+ * Module dependencies
+ */
+var util = require( './util' );
+var assert = require( 'assert' );
+
+/**
+ * Testing data
+ */
+var fixture = require( './fixture' );
+
+describe( 'wpcom.domains', function() {
+	// Global instances
+	const wpcom = util.wpcom();
+	const domains = wpcom.domains();
+
+	describe( 'wpcom.domains.suggestions', function() {
+		it( 'should request domains passing a string as query', done => {
+			domains.suggestions( fixture.queryDomains.query )
+				.then( resp => {
+					assert.ok( resp );
+					assert.equal( 'number', typeof resp.length );
+
+					done();
+				} )
+				.catch( done );
+		} );
+
+		it( 'should request domains passing an object as query', done => {
+			domains.suggestions( fixture.queryDomains )
+				.then( resp => {
+					assert.ok( resp );
+					assert.equal( 'number', typeof resp.length );
+
+					done();
+				} )
+				.catch( done );
+		} );
+	} );
+
+	describe( 'wpcom.domains.suggestionsExamples', function() {
+		it( 'should request domains using the example endpoint', done => {
+			domains.suggestions( fixture.queryDomains.query )
+				.then( resp => {
+					assert.ok( resp );
+					assert.equal( 'number', typeof resp.length );
+					done();
+				} )
+				.catch( done );
+		} );
+	} );
+
+	describe( 'wpcom.domains.supportedStates', function() {
+		it( 'should get localized list of supported states of Spain', done => {
+			domains.supportedStates( 'es' )
+				.then( resp => {
+					assert.ok( resp );
+					done();
+				} )
+				.catch( done );
+		} );
+	} );
+
+	describe( 'wpcom.domains.supportedCountries', function() {
+		it( 'should get localized list of supported countries', done => {
+			domains.supportedCountries()
+				.then( resp => {
+					assert.ok( resp );
+					done();
+				} )
+				.catch( done );
+		} );
+	} );
+} );
