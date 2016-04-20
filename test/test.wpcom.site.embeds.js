a1edc38384de83243b6a900fc061273940db3a88
retrofox <rdsuarez@gmail.com>
2016-04-20 20:16:35 -0300

    test: update site embeds tests

--- test/test.wpcom.site.embeds.js
+++ test/test.wpcom.site.embeds.js
@@ -9,14 +9,28 @@ var assert = require( 'assert' );
  */
 var fixture = require( './fixture' );
 
-describe( 'wpcom.site.embeds', function() {
+describe( 'wpcom.site.embed', function() {
 	// Global instances
 	var wpcom = util.wpcom();
 	var site = wpcom.site( util.site() );
 
-	describe( 'wpcom.site.renderEmbed(\'embed\' )', function() {
+	describe( 'wpcom.site.renderEmbed', function() {
 		it( 'should render embed', done => {
 			site.renderEmbed( fixture.embed )
+				.then( data => {
+					assert.equal( data.embed_url, fixture.embed );
+					assert.ok( data.result );
+					done();
+				} )
+				.catch( done );
+		} );
+	} );
+
+	describe( 'wpcom.site.embed.render', function() {
+		it( 'should render embed', done => {
+			var embed = site.embed( fixture.embed );
+
+			embed.render()
 				.then( data => {
 					assert.equal( data.embed_url, fixture.embed );
 					assert.ok( data.result );
